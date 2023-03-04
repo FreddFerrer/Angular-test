f26a68233626f28ef856bebbca770519758dfc81
Freddy <ferreirafreddy39@gmail.com>
2023-03-03 23:16:33 -0300

    conexion a bd realizada

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,29 @@ export class InfoPaginaService {
 
   info: InfoPaginaI = {};
   cargada: boolean = false;  
+  equipo: any[] = [];
 
   constructor(private httpClient: HttpClient) {
-  
-    console.log('service info pagina cargada');
-
-    this.httpClient.get('assets/data/data-pagina.json')
-      .subscribe( (response: InfoPaginaI) => {
-        //SI YO ACLARO DE QUE TIPO ES LA RESPUESTA PUEDO ACCEDER A LAS PROPIEDADES DEL JSON
-        this.cargada = true;
-        this.info = response;
-        console.log(response);
-      })
-
+    this.cargarInfo();
+    this.cargarEquipo();
    }
+
+  private cargarInfo(){
+    this.httpClient.get('assets/data/data-pagina.json')
+    .subscribe( (response: InfoPaginaI) => {
+    //SI YO ACLARO DE QUE TIPO ES LA RESPUESTA PUEDO ACCEDER A LAS PROPIEDADES DEL JSON
+    this.cargada = true;
+    this.info = response;
+    
+    })
+   }
+
+  private cargarEquipo(){
+    this.httpClient.get('https://angular-html-20583-default-rtdb.asia-southeast1.firebasedatabase.app/.json')
+    .subscribe( (res: { equipo: any[] }) => {
+      this.equipo = res.equipo;
+      console.log('funciona???');
+      console.log(res);
+    })
+  }
 }
